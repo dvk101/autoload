bf7d2c5766797f0591e45aefb370abc7252e2ff8
david <165060772+dvk101@users.noreply.github.com>
2024-04-03 13:15:53 +0300

    okcancel

--- autorun.py
+++ autorun.py
@@ -8,10 +8,10 @@ import atexit
 messagebox.showinfo(
     title="autorun.py",
     message="run code setup?",
-    type="yesno")
+    type="OKCANCEL")
 
 ## answer
-if messagebox.YES:
+if messagebox.OK:
    response = os.startfile("C:/Users/User/AppData/Local/GitHubDesktop/GitHubDesktop.exe"), os.startfile("C:/Users/User/AppData/Local/Programs/Microsoft VS Code/Code.exe") ## to add an app enter os.startfile("yourappsroot/yourapp.exe"), if theres 2 or more, repeat the os.startfile after a comma
    print(response)
 else:
