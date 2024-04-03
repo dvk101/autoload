94ee26832f206d4e1d9cf2e0a129845407853343
david <DavidKolyadaAI@gmail.com>
2024-04-03 18:51:06 +0300

    fffe

--- autorun.py
+++ autorun.py
@@ -7,7 +7,7 @@ from tkinter import messagebox
 messagebox.showinfo(
     title="autorun.py",
     message="run code setup?",
-    type="YESNO")
+    type="yesno")
 
 
 
@@ -16,8 +16,8 @@ messagebox.showinfo(
 response = messagebox.YESNO("confirm","launch code setup?")
 
 if response:
-   os.system("C:/Users/User/AppData/Local/GitHubDesktop/GitHubDesktop.exe")
-   os.system("C:/Users/User/AppData/Local/Programs/Microsoft VS Code/Code.exe")
+   #os.system("C:/Users/User/AppData/Local/GitHubDesktop/GitHubDesktop.exe")
+   #os.system("C:/Users/User/AppData/Local/Programs/Microsoft VS Code/Code.exe")
    ## to add an app enter os.system("yourappsroot/yourapp.exe"), if theres 2 or more, repeat the os.startfile after a comma
    print("launched")
 else:
