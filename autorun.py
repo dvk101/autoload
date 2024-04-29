b6731ef6c869da1f1036fc2dbff16b0ef15b671e
dvk101 <DavidKolyadaAI@gmail.com>
2024-04-29 13:13:14 +0300

    PABEDA!!!

--- autorun.py
+++ autorun.py
@@ -7,11 +7,10 @@ from tkinter import messagebox
 reply = messagebox.askyesno(title="autorun.py", message="run setup?")
 
 if reply == True:
-   os.system("C:/Users/User/AppData/Local/GitHubDesktop/GitHubDesktop.exe")
-   os.system("C:/Program Files/Mozilla Firefox/firefox.exe")
-   ## to add an app enter os.system("yourappsroot/yourapp.exe")
+   os.startfile("C:/Users/User/AppData/Local/GitHubDesktop/GitHubDesktop.exe")
+   os.startfile("C:/Program Files/Mozilla Firefox/firefox.exe")
+   ## to add an app enter os.startfile("yourappsroot/yourapp.exe")
    print("launched")
-   quit()
 elif reply == False:
    print("quit")
    quit()
